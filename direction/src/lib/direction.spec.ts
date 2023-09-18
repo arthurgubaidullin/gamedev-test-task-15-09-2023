e523b30ad2a363cd740e2ec0f0c71bcaaca17f54
Artur Gubaidullin <lapteuh@gmail.com>
2023-09-18 10:14:02 +0000

    Add type Direction

--- direction/src/lib/direction.spec.ts
+++
@@ -1,7 +0,0 @@
-import { direction } from './direction';
-
-describe('direction', () => {
-  it('should work', () => {
-    expect(direction()).toEqual('direction');
-  });
-});
--- direction/src/lib/direction.ts
+++ direction/src/lib/direction.ts
@@ -1,3 +1,6 @@
-export function direction(): string {
-  return 'direction';
-}
+export type Direction = typeof RIGHT | typeof LEFT | typeof UP | typeof DOWN;
+
+export const RIGHT = 'right' as const;
+export const LEFT = 'left' as const;
+export const UP = 'up' as const;
+export const DOWN = 'down' as const;
