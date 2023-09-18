c65e7c29f85323a6ac821e8c0a882372b08cb431
Artur Gubaidullin <lapteuh@gmail.com>
2023-09-18 10:17:20 +0000

    Add type Item

--- item/src/lib/item.ts
+++ item/src/lib/item.ts
@@ -1,3 +1,18 @@
-export function item(): string {
-  return 'item';
-}
+import * as O from 'fp-ts/Option';
+
+export type Item = Readonly<{
+  state: 'idle' | 'destroyed';
+}>;
+
+export const create = (): Item => {
+  return { state: 'idle' };
+};
+
+export const hit = (item: Item): O.Option<Item> => {
+  if (item.state === 'idle') {
+    return O.some({
+      state: 'destroyed',
+    });
+  }
+  return O.none;
+};
