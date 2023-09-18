989c88f270841d3d8bd1fe06a62d94d5cc8f8461
Artur Gubaidullin <lapteuh@gmail.com>
2023-09-18 11:58:50 +0000

    Add type XYKey

--- xy-key/src/lib/xy-key.spec.ts
+++
@@ -1,7 +0,0 @@
-import { xyKey } from './xy-key';
-
-describe('xyKey', () => {
-  it('should work', () => {
-    expect(xyKey()).toEqual('xy-key');
-  });
-});
--- xy-key/src/lib/xy-key.ts
+++ xy-key/src/lib/xy-key.ts
@@ -1,3 +1,13 @@
-export function xyKey(): string {
-  return 'xy-key';
-}
+import * as _Eq from 'fp-ts/Eq';
+
+export type XYKey = `${string}-${string}`;
+
+export const create = (data: Readonly<{ x: number; y: number }>): XYKey =>
+  `${data.x.toString()}-${data.y.toString()}` as const;
+
+export const parse = (key: XYKey): Readonly<{ x: number; y: number }> => {
+  const [x, y] = key.split('-');
+  return { x: parseInt(x), y: parseInt(y) };
+};
+
+export const Eq: _Eq.Eq<XYKey> = _Eq.eqStrict;
