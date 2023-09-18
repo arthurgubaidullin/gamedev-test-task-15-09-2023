bf31e9b5fb4fc52d927290e39097f5a12d7227fa
Artur Gubaidullin <lapteuh@gmail.com>
2023-09-18 13:12:40 +0000

    Reuse const

--- game-app/src/target/target.tsx
+++ game-app/src/target/target.tsx
@@ -1,17 +1,18 @@
 import * as PixiGraphics from '@pixi/graphics';
 import { Container, Graphics } from '@pixi/react';
 import { useCallback } from 'react';
+import { CELL_SIZE } from '../game/constants';
 
 export function Target(data: { x: number; y: number }) {
   const draw = useCallback((g: PixiGraphics.Graphics) => {
     g.clear();
     g.lineStyle(2, 0, 1);
     g.beginFill(16711867, 0.25);
-    g.drawRoundedRect(0, 0, 62, 62, 10);
+    g.drawRoundedRect(0, 0, CELL_SIZE, CELL_SIZE, 10);
     g.endFill();
   }, []);
   return (
-    <Container x={data.x * 62} y={data.y * 62}>
+    <Container x={data.x * CELL_SIZE} y={data.y * CELL_SIZE}>
       <Graphics draw={draw} />
     </Container>
   );
