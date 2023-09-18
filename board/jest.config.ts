07b7c425c978db8d1dc656b8b4332e60a9035dc1
Artur Gubaidullin <lapteuh@gmail.com>
2023-09-18 10:11:58 +0000

    Rename project

--- board/jest.config.ts
+++ board/jest.config.ts
@@ -1,10 +1,10 @@
 /* eslint-disable */
 export default {
-  displayName: 'game-model',
+  displayName: 'board',
   preset: '../jest.preset.js',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../coverage/game-model',
+  coverageDirectory: '../coverage/board',
 };
