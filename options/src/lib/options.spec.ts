d5e016060bff40c6826d760fd61851155e8d1db4
Artur Gubaidullin <lapteuh@gmail.com>
2023-09-18 11:53:34 +0000

    Add type Options

--- options/src/lib/options.spec.ts
+++
@@ -1,7 +0,0 @@
-import { options } from './options';
-
-describe('options', () => {
-  it('should work', () => {
-    expect(options()).toEqual('options');
-  });
-});
--- options/src/lib/options.ts
+++ options/src/lib/options.ts
@@ -1,3 +1,10 @@
-export function options(): string {
-  return 'options';
+export interface Options {
+  readonly rows: number;
+  readonly columns: number;
 }
+
+export const create = (rows: number, columns: number): Options =>
+  ({
+    rows,
+    columns,
+  } as const);
