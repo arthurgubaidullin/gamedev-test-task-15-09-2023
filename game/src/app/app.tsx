db6d110bc07cdbb7cc0432990f101b56c4b6a64b
Artur Gubaidullin <lapteuh@gmail.com>
2023-09-18 12:39:37 +0000

    Fix component App

--- game/src/app/app.tsx
+++ game/src/app/app.tsx
@@ -1,8 +1,27 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { AppProvider } from '@pixi/react';
+import { Application } from 'pixi.js';
+import { useMemo } from 'react';
 import './app.module.css';
+import { Game } from '../game/game';
 
 export function App() {
-  return <div />;
+  const app = useMemo(() => new Application(), []);
+
+  return (
+    <div
+      style={{
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <AppProvider value={app}>
+        <Game />
+      </AppProvider>
+    </div>
+  );
 }
 
 export default App;
