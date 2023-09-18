35d2b515d43935c008f74a52d76882f82bf5d6a8
Artur Gubaidullin <lapteuh@gmail.com>
2023-09-18 12:43:16 +0000

    Move project

--- game-model/xy-key/jest.config.ts
+++ game-model/xy-key/jest.config.ts
@@ -1,10 +1,10 @@
 /* eslint-disable */
 export default {
   displayName: 'xy-key',
-  preset: '../jest.preset.js',
+  preset: '../../jest.preset.js',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../coverage/xy-key',
+  coverageDirectory: '../../coverage/game-model/xy-key',
 };
