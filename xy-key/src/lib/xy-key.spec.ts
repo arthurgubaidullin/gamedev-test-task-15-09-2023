edeb42d00ad1687ecf9050516a2106063732a830
Artur Gubaidullin <lapteuh@gmail.com>
2023-09-18 11:57:49 +0000

    Add project xy-key

---
+++ xy-key/src/lib/xy-key.spec.ts
@@ -0,0 +1,7 @@
+import { xyKey } from './xy-key';
+
+describe('xyKey', () => {
+  it('should work', () => {
+    expect(xyKey()).toEqual('xy-key');
+  });
+});
---
+++ xy-key/src/lib/xy-key.ts
@@ -0,0 +1,3 @@
+export function xyKey(): string {
+  return 'xy-key';
+}
